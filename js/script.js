0b722cbf330396fd493c6e58395e1c8f9fa73b6f
Adniloet Sosa <adni.sori@gmail.com>
2024-11-05 22:21:23 +0100

    JS realizado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
  // Aquí tu código
+ const botonAgregar = document.getElementById('agregar');
+ const listado = document.getElementById('lista');
+ //const element = prompt('Agrega un elemento a la lista');
+ 
+ 
+ 
+ botonAgregar.addEventListener('click', function() {
+     const item = prompt('Agrega un elemento a la lista');
+     const newElement = document.createElement('li');
+     listado.appendChild(newElement);
+     const elementText = document.createTextNode(item);
+     newElement.appendChild(elementText);
+ });
+ 
